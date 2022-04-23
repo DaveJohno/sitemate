381f37c0a790914138bae3c9ae9fdeb6be79f102
DaveJohno <88128050+DaveJohno@users.noreply.github.com>
2022-04-23 13:00:12 +1000

    get request working

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3456, () => {
-  console.log(`server listening on port 4567`);
+port = 3456;
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
 
 //parses json body and it assigns it to req.body so we can use it
@@ -18,4 +20,11 @@ app.get(`/`, (req, res) => {
   <h1>Welcome to SiteMate API Test</h1>
   <p>Talk to our Api by using /api/data</p>
   `);
+});
+
+app.get("/api/data", (req, res) => {
+  console.log(res.data);
+  res.json({
+    apiData,
+  });
 });
